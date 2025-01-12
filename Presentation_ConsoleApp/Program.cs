8de691921c0ef91c330ad8f7631c55825d663ce9
Racsoluas <oscar.funes.galindo@gmail.com>
2025-01-12 12:51:07 +0100

    Fjärde försöket

---
+++ Presentation_ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Buisness.Interfaces;
+using Buisness.Services;
+using Presentation_ConsoleApp.Dialogs;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddSingleton<IFileService>(new FileService("users.json"));
+serviceCollection.AddSingleton<IUserService, UserService>();
+serviceCollection.AddSingleton<MenuDialogs>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var menuDialogs = serviceProvider.GetRequiredService<MenuDialogs>();
+
+menuDialogs.MainMenu();
